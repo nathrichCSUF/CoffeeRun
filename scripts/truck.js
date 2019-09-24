4cd7b5e9029fee989fe0eef491d0c49f222aa597
Nathaniel Richards <nathrich23@csu.fullerton.edu>
2019-09-23 23:12:25 -0700

    Finished Chapter 8

--- scripts/truck.js
+++ scripts/truck.js
@@ -1,4 +1,4 @@
-(function (window) {
+(function(window) {
   "use strict";
   var App = window.App || {};
 
@@ -6,16 +6,22 @@
     this.truckId = truckId;
     this.db = db;
   }
-
-  Truck.prototype.createOrder = function (order) {
-    console.log("Adding order for " + order.emailAddress);
-    this.db.add(order.emailAddress, order);
+  Truck.prototype.createOrder = function(order) {
+    console.log("Adding order for " + order.email);
+    this.db.add(order.email, order);
   };
-  Truck.prototype.deliverOrder = function (customerId) {
+  Truck.prototype.deliverOrder = function(customerId) {
     console.log("Delivering order for " + customerId);
     this.db.remove(customerId);
   };
+  Truck.prototype.printOrders = function() {
+    var customerIdArray = Object.keys(this.db.getAll());
+    console.log("Truck #" + this.truckId + " has pending orders:");
+    customerIdArray.forEach(function(id) {
+      console.log(this.db.get(id));
+    }.bind(this));
+  };
+
   App.Truck = Truck;
   window.App = App;
-
 })(window);
